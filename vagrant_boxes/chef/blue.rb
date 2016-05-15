c0cc0405db46dd750b80240dc7ee53fc6d82a9d9
unknown <Administrator@BLRW3003945.internal.imsglobal.com>
2016-05-15 06:55:16 +0530

    Added sample scripts

---
+++ vagrant_boxes/chef/blue.rb
@@ -0,0 +1,21 @@
+cookbook_file '/tmp/comingfromblue.txt' do
+  action :create
+  path 'path'
+  source 'iamlocatedinthefiledirectory.txt'
+ end
+
+ packages=['tomcat','tomcat7-docs','tomcat7-admin','tomcat7-examples']
+
+packages.each{|take_each_package_in_packages_array|
+
+
+package #{take_each_package_in_packages_array} do
+  action :install
+end
+
+}
+
+service 'tomcat' do
+  supports :status => true, :restart => true, :reload => true
+  action [:start, :enable]
+end
