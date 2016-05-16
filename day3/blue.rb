d9d9f77c100cb1971502b8237c6ea60897896a8f
unknown <Administrator@BLRW3003945.internal.imsglobal.com>
2016-05-16 09:02:49 +0530

    Few more instructions added

--- day3/blue.rb
+++ day3/blue.rb
@@ -1,21 +1,20 @@
 cookbook_file '/tmp/comingfromblue.txt' do
   action :create
-  path 'path'
   source 'iamlocatedinthefiledirectory.txt'
  end
 
- packages=['tomcat','tomcat7-docs','tomcat7-admin','tomcat7-examples']
+ packages=['tomcat7','tomcat7-docs','tomcat7-admin','tomcat7-examples']
 
 packages.each{|take_each_package_in_packages_array|
 
 
-package #{take_each_package_in_packages_array} do
+package take_each_package_in_packages_array do
   action :install
 end
 
 }
 
-service 'tomcat' do
+service 'tomcat7' do
   supports :status => true, :restart => true, :reload => true
   action [:start, :enable]
 end
